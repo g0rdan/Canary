79920c4e1a4cfff78d9f8a7ac5b05414c3c082ad
Denis Gordin <gordin.dan@gmail.com>
2018-03-07 18:43:42 +0300

    [Battery] Add Battery.iOS project

---
+++ src/Battery/Battery.iOS/Battery.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+namespace Canary.Battery.iOS
+{
+    public class Battery : IBattery
+    {
+        public bool IsCharging => UIDevice.CurrentDevice.BatteryState == UIDeviceBatteryState.Charging;
+
+        public float BatteryLevel => UIDevice.CurrentDevice.BatteryLevel;
+
+        public ChargingState BatteryState
+        {
+            get
+            {
+                switch (UIDevice.CurrentDevice.BatteryState)
+                {
+                    case UIDeviceBatteryState.Unknown:
+                        return ChargingState.Unknown;
+                    case UIDeviceBatteryState.Unplugged:
+                        return ChargingState.Discharging;
+                    case UIDeviceBatteryState.Charging:
+                        return ChargingState.Charging;
+                    case UIDeviceBatteryState.Full:
+                        return ChargingState.Full;
+                    default:
+                        return ChargingState.Unknown;
+                }
+            }
+        }
+
+        public PowerSourceType PowerSource
+        {
+            get
+            {
+                switch (UIDevice.CurrentDevice.BatteryState)
+                {
+                    case UIDeviceBatteryState.Unknown:
+                        return PowerSourceType.Unknown;
+                    case UIDeviceBatteryState.Unplugged:
+                        return PowerSourceType.Battery;
+                    case UIDeviceBatteryState.Charging:
+                        return PowerSourceType.AC;
+                    case UIDeviceBatteryState.Full:
+                        return PowerSourceType.AC;
+                    default:
+                        return PowerSourceType.Unknown;
+                }
+            }
+        }
+    }
+}
