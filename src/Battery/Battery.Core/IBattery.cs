79b6b6494b798356626d0f802625aecb40111adc
Denis Gordin <gordin.dan@gmail.com>
2018-03-07 16:14:21 +0300

    [Battery] Add IBattery.cs

---
+++ src/Battery/Battery.Core/IBattery.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Canary.Battery
+{
+    interface IBattery
+    {
+        /// <summary>
+        /// Shows state of charging. True if it's plugged to AC/USB
+        /// </summary>
+        bool IsCharging { get; }
+        /// <summary>
+        /// Shows battery level from 0.0 to 1.0 where 1.0 it's 100% charged.
+        /// </summary>
+        float BatteryLevel { get; }
+        /// <summary>
+        /// Gets charging state of battery
+        /// </summary>
+        ChargingState BatteryState { get; }
+    }
+
+    public enum ChargingState
+    {
+        Unknown,
+        Unplugged,
+        Charging,
+        Full
+    }
+}
