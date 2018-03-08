591898bf2cdfebe85b5dbdc2b2106494aebc247d
Denis Gordin <gordin.dan@gmail.com>
2018-03-08 18:20:00 +0300

    [Batary] Add Droid tests

---
+++ src/Battery/Tests/Battery.Droid.Tests/Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Canary.Battery;
+using NUnit.Framework;
+
+namespace Battery.Droid.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        [Test]
+        public void CheckChargingOfBattery()
+        {
+            var batteryProvider = new Canary.Battery.Droid.Battery();
+            Assert.True(batteryProvider.IsCharging);
+        }
+
+        [Test]
+        public void CheckBatteryLevel()
+        {
+            var batteryProvider = new Canary.Battery.Droid.Battery();
+            Assert.True(batteryProvider.BatteryLevel >= 0 && batteryProvider.BatteryLevel <= 1);
+        }
+
+        [Test]
+        public void CheckBatteryPowerType()
+        {
+            var batteryProvider = new Canary.Battery.Droid.Battery();
+            // if we're running on a real device it should be plugged and Apple SDK does not
+            // provide AC/USB thing. We can see only as AC.
+            Assert.True(batteryProvider.PowerSource == PowerSourceType.AC);
+        }
+
+        [Test]
+        public void CheckBatteryState()
+        {
+            var batteryProvider = new Canary.Battery.Droid.Battery();
+            Assert.True(batteryProvider.BatteryState == ChargingState.Charging || batteryProvider.BatteryState == ChargingState.Full);
+        }
+    }
+}
