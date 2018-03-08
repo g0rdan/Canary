78ec40f94305e32486032b79bbd07056472912e8
Denis Gordin <gordin.dan@gmail.com>
2018-03-08 17:55:04 +0300

    [Batary] Add iOS tests

---
+++ src/Battery/Tests/Battery.iOS.Tests/Tests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+using Canary.Battery;
+
+namespace Battery.iOS.Tests
+{
+    /// <summary>
+    /// These tests should run only on a real devices
+    /// </summary>
+    [TestFixture]
+    public class Tests
+    {        
+        [Test]
+        public void CheckChargingOfBattery()
+        {
+            var batteryProvider = new Canary.Battery.iOS.Battery();
+            Assert.True(batteryProvider.IsCharging);
+        }
+
+        [Test]
+        public void CheckBatteryLevel()
+        {
+            var batteryProvider = new Canary.Battery.iOS.Battery();
+            Assert.True(batteryProvider.BatteryLevel >= 0 && batteryProvider.BatteryLevel <= 1);
+        }
+
+        [Test]
+        public void CheckBatteryPowerType()
+        {
+            var batteryProvider = new Canary.Battery.iOS.Battery();
+            // if we're running on a real device it should be plugged and Apple SDK does not
+            // provide AC/USB thing. We can see only as AC.
+            Assert.True(batteryProvider.PowerSource == PowerSourceType.AC);
+        }
+
+        [Test]
+        public void CheckBatteryState()
+        {
+            var batteryProvider = new Canary.Battery.iOS.Battery();
+            Assert.True(batteryProvider.BatteryState == ChargingState.Charging || batteryProvider.BatteryState == ChargingState.Full);
+        }
+    }
+}
