f92082c4e97570bb0b3bbd3daca317f96e06f1ee
Denis Gordin <gordin.dan@gmail.com>
2018-03-13 12:29:53 +0300

    [SoC] Add simple core project

---
+++ src/SoC/SoC.Core/ICnrSoC.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoC.Core
+{
+    public interface ICnrSoC
+    {
+        /// <summary>
+        /// Name of chip
+        /// </summary>
+        string Model { get; }
+        /// <summary>
+        /// Current usage of central processor (from 0 to 1 in float) 
+        /// </summary>
+        float Usage { get; }
+        /// <summary>
+        /// Current frequency in MHz
+        /// </summary>
+        float Frequency { get; }
+        /// <summary>
+        /// Amount of cores in SoC
+        /// </summary>
+        int Cores { get; }
+        /// <summary>
+        /// This property needs to show some additional
+        /// information about SoC on a device, which could not be
+        /// represent as common information for many platforms.
+        /// </summary>
+        IList<(string Key, string Value, string Description)> AdditionalInformation { get; }
+    }
+}
