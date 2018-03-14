df5f7396ebecc3f4caeb53934fec6cc774d848d7
Denis Gordin <gordin.dan@gmail.com>
2018-03-14 22:47:03 +0300

    [SoC] Empty iOS project

---
+++ src/SoC/SoC.iOS/CnrSoC.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Canary.SoC.iOS
+{
+    public class CnrSoC : ICnrSoC
+    {
+        public CnrSoC()
+        {
+        }
+
+        public string Model => throw new NotImplementedException();
+
+        public float CurrentFrequency => throw new NotImplementedException();
+
+        public float MinFrequency => throw new NotImplementedException();
+
+        public float MaxFrequency => throw new NotImplementedException();
+
+        public int Cores => throw new NotImplementedException();
+
+        public Task<IList<(string Key, string Value, string Description)>> AdditionalInformation(CancellationTokenSource cts = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<float> Usage(CancellationTokenSource cts = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
